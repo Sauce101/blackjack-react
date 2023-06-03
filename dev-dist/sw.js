3a7273da3dd11d16787c7c135ba96bc91c8a1921
Sauce101 <42197557+Sauce101@users.noreply.github.com>
2023-06-03 14:42:29 -0700

    dev-dist sw  revision

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.76jik8oi7s"
+    "revision": "0.0tk5d7v7bj8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
